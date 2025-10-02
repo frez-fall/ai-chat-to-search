342d84ce7de943b81cd6ae8940a6a85347b49b0d
Frezneal Fallaria <115141272+frez-fall@users.noreply.github.com>
2025-10-03 02:20:51 +0800

    Delete backend/next.config.js

--- backend/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: { ignoreBuildErrors: true }, // optional while stabilizing
-  eslint: { ignoreDuringBuilds: true }     // optional while stabilizing
-};
-
-module.exports = nextConfig;
